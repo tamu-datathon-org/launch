ffd39f406a88a2cd59188b7d9417e46d9360bc76
Samarth Dave <sdave@tamu.edu>
2020-10-25 22:18:40 -0400

    Successfully connect to firebase db

---
+++ firebaseTest.js
@@ -0,0 +1,15 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./service-account-file.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://project-f271e.firebaseio.com/"
+});
+
+// As an admin, the app has access to read and write all data, regardless of Security Rules
+var db = admin.database();
+var ref = db.ref("restricted_access/secret_document");
+ref.once("value", function(snapshot) {
+  console.log(snapshot.val());
+});
