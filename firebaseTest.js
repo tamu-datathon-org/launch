f05c8cb301f642855e139c934dc745a6aae6c293
Samarth Dave <sdave@tamu.edu>
2020-10-26 14:20:17 -0400

    Use firestore

--- firebaseTest.js
+++ firebaseTest.js
@@ -1,15 +1,18 @@
-var admin = require("firebase-admin");
+// https://firebase.google.com/docs/firestore/quickstart
+const admin = require('firebase-admin');
 
-var serviceAccount = require("./service-account-file.json");
+const serviceAccount = require("./service-account-file.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: "https://project-f271e.firebaseio.com/"
 });
 
-// As an admin, the app has access to read and write all data, regardless of Security Rules
-var db = admin.database();
-var ref = db.ref("restricted_access/secret_document");
-ref.once("value", function(snapshot) {
-  console.log(snapshot.val());
-});
+const db = admin.firestore();
+
+// (async () => {
+//   const snapshot = await db.collection('users').get();
+//   snapshot.forEach((doc) => {
+//     console.log(doc.id, '=>', doc.data());
+//   });
+// })();
