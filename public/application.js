70f2bb135d5cc149b504ba8cc517c3bc89fcf369
Samarth Dave <sdave@tamu.edu>
2020-11-09 23:05:44 -0600

    Break code into script files + load github repos

--- public/application.js
+++ public/application.js
@@ -1,59 +1,3 @@
-/**
- * Function that returns the link for top languages from username
- * @param {string} username Github handle
- */
-const getTopLanguagesLink = (username) => {
-    if (!username) {
-        throw new Error('"username" is not defined even though it should be');
-    }
-
-    return `https://github-readme-stats.vercel.app/api/top-langs/?username=${username}`;
-};
-
-/**
- * Function that returns DATA for top languages (HTML)
- * @param {string} username Github handle
- */
-const getTopLanguagesData = async (username) => {
-    const dataURL = getTopLanguagesLink(username);
-
-    const result = await fetch(dataURL);
-
-    if (result) {
-        return result;
-    } else {
-        throw new Error('Invalid data response - fetching top languages data');
-    }
-};
-
-/**
- * Function that returns link for stats information for repositories
- * @param {string} username Github handle
- */
-const getStatsCardLink = (username) => {
-    if (!username) {
-        throw new Error('"username" is not defined even though it should be');
-    }
-
-    return `https://github-readme-stats.vercel.app/api?username=${username}`;
-};
-
-/**
- * Function that returns DATA for top languages (HTML)
- * @param {string} username Github handle
- */
-const getStatsCardData = async (username) => {
-    const dataURL = getStatsCardLink(username);
-
-    const result = await fetch(dataURL);
-
-    if (result) {
-        return result;
-    } else {
-        throw new Error('Invalid data response - fetching Stats data');
-    }
-};
-
 // when github input loses focus
 $("#gitHub").focusout(async () => {
     let inputPath = $("#gitHub").val().toLowerCase();
@@ -76,4 +20,53 @@ $("#gitHub").focusout(async () => {
         console.log(data[0]);
         console.log(data[1]);
     }
+});
+
+// ==============================
+// code originally from admin.ejs
+// ==============================
+
+$('tr').on('dblclick', async () => {
+    $('#addModal').modal('show');
+
+    // -------------------------------------
+    // set image previews to github previews
+    // -------------------------------------
+    const githubLink = $('#github-link-modal').attr('href');
+    // use function from ./partial/scripts (application.js)
+
+    const GithubURL = new URL(githubLink);
+
+    // parse username from URL
+    const pathArray = GithubURL.pathname.split('/').filter(e => e.length > 0);
+    if (pathArray.length == 0) {
+        throw new Error(`Invalid path submitted to form: ${githubLink}`);
+    }
+    const username = pathArray[0];
+
+    // get links from username
+    const StatsLink        = getStatsCardLink(username);
+    const TopLanguagesLink = getTopLanguagesLink(username);
+
+    // set both img "attr" src values
+    $('#github-stats-card').attr('src', StatsLink);
+    $('#github-languages-card').attr('src', TopLanguagesLink);
+
+    // ==================================================
+    // make request to get github data once modal clicked
+    // ==================================================
+
+    const resp = await fetch(`/apply/hello/github/${username}/repos`);
+    const data = await resp.json();
+
+    // only get first five repos
+    const arr = data.slice(0,5);
+
+    arr.forEach((elem) => {
+        $("#github-repos-list").prepend(
+            `<li>
+                <a href="${elem.html_url}">${elem.name}</a>
+            </li>`
+        );
+    });
 });
