2bcad1b0795a4eb5f51ad599e8b1753d2eddabbb
Samarth Dave <sdave@tamu.edu>
2020-11-20 14:55:47 -0600

    Show user data on admin page

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,25 @@
 const express = require("express");
 const router = express.Router();
 
+const applicationService = require('./../../services/ApplicationService');
+
 /**
  * Render the "admin" page for organizers and sponsors
  * Route: /apply/admin
  */
-router.get("/", (req, res) => {
-    return res.render("admin");
+router.get("/", async (req, res) => {
+    const data = await applicationService.getApplicationsPreview();
+    return res.render("admin", { data });
 });
+
+/**
+ * Render a json preview of the first 10 applications
+ * Route: /apply/preview
+ * Used for table view
+ */
+// router.get('/preview', async(req, res) => {
+//     const data = await applicationService.getApplicationsPreview();
+//     res.send(data);
+// });
 
 module.exports = router;
